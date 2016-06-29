2a43b3fd1205bd756fd0f97b397f4595082cfe39
Bas de Vries <mail@basdevri.es>
2016-06-29 21:14:22 +0200

    compile to es5

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-export function normalizePhoneNumber(phoneNumber) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.normalizePhoneNumber = normalizePhoneNumber;
+exports.addPrefixToNumber = addPrefixToNumber;
+function normalizePhoneNumber(phoneNumber) {
   // filter all ASCII chars from the string.
-  const _phoneNumber = phoneNumber.toString();
-  const nonASCIIPhone = _phoneNumber.replace(/[^\x00-\x7F]/g, '');
+  var _phoneNumber = phoneNumber.toString();
+  var nonASCIIPhone = _phoneNumber.replace(/[^\x00-\x7F]/g, '');
 
-  const validTokens = ['0','1','2','3','4','5','6','7','8','9','+'];
-  let cleanNumber = '';
+  var validTokens = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+'];
+  var cleanNumber = '';
 
-  const phoneArr = nonASCIIPhone.split('');
-  phoneArr.map(token => {
-  if (validTokens.indexOf(token) > -1) {
-    cleanNumber += token;
-  }
+  var phoneArr = nonASCIIPhone.split('');
+  phoneArr.map(function (token) {
+    if (validTokens.indexOf(token) > -1) {
+      cleanNumber += token;
+    }
   });
 
   return cleanNumber;
@@ -22,21 +29,25 @@ export function normalizePhoneNumber(phoneNumber) {
 @discussion: number needs to be 'clean' first. It won't registrer
 the following: (06)1234556.
 */
-export function addPrefixToNumber(phoneNumber, prefix) {
-  const _phoneNumber = phoneNumber.toString();
+function addPrefixToNumber(phoneNumber, prefix) {
+  var _phoneNumber = phoneNumber.toString();
   if (_phoneNumber.length <= 0) throw new Error('phoneNumber must have a length > 0');
   if (!prefix) throw new Error('Missing prefix parameter in addPrefixToNumber');
 
-  let numberWithPrefix = phoneNumber;
-  if (_phoneNumber.substring(0,2) == '06' && prefix) { // accept ints and strings
-   numberWithPrefix = `+316${phoneNumber.substring(2, _phoneNumber.length)}`;
-  } else if (_phoneNumber.substring(0, 5) == '+3106') { // only works with nl for now, need more declarative solution
-   numberWithPrefix = `+316${phoneNumber.substring(5, _phoneNumber.length)}`;
-  } else if (_phoneNumber.substring(0,5) == '00316') { // only works with nl for now, need more declarative solution
-   numberWithPrefix = `+316${phoneNumber.substring(5, _phoneNumber.length)}`;
- } else if (_phoneNumber.substring(0,5) === '+0316') {
-   numberWithPrefix = `+316${phoneNumber.substring(5, _phoneNumber.length)}`
- }
+  var numberWithPrefix = phoneNumber;
+  if (_phoneNumber.substring(0, 2) == '06' && prefix) {
+    // accept ints and strings
+    numberWithPrefix = '+316' + phoneNumber.substring(2, _phoneNumber.length);
+  } else if (_phoneNumber.substring(0, 5) == '+3106') {
+    // only works with nl for now, need more declarative solution
+    numberWithPrefix = '+316' + phoneNumber.substring(5, _phoneNumber.length);
+  } else if (_phoneNumber.substring(0, 5) == '00316') {
+    // only works with nl for now, need more declarative solution
+    numberWithPrefix = '+316' + phoneNumber.substring(5, _phoneNumber.length);
+  } else if (_phoneNumber.substring(0, 5) === '+0316') {
+    numberWithPrefix = '+316' + phoneNumber.substring(5, _phoneNumber.length);
+  }
 
   return numberWithPrefix;
 }
+
